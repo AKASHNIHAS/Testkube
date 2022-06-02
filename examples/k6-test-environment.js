e106e8e380d7a4e8e4acd7ed2fa9070435819f16
Akash LM <97584884+AKASHNIHAS@users.noreply.github.com>
2022-06-02 11:33:27 +0530

    Update k6-test-environment.js

--- examples/k6-test-environment.js
+++ examples/k6-test-environment.js
@@ -2,6 +2,6 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get(`https://${__ENV.TARGET_HOSTNAME}/testkube/`);
+  http.get(`https://${__ENV.TARGET_hostname}/testkube/`);
   sleep(1);
 }
